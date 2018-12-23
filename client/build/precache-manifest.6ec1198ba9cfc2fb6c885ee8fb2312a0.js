caf36749d1dc6b422421584b817bbc7a22707cbb
tech-boy <darshak_mehta@live.com>
2018-12-23 16:51:36 -0500

    BruTap

---
+++ client/build/precache-manifest.6ec1198ba9cfc2fb6c885ee8fb2312a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c309d7b970c80561f7c",
+    "url": "/static/js/main.6c309d7b.chunk.js"
+  },
+  {
+    "revision": "d98351587c2fffbd7c68",
+    "url": "/static/js/1.d9835158.chunk.js"
+  },
+  {
+    "revision": "6c309d7b970c80561f7c",
+    "url": "/static/css/main.ba89f0eb.chunk.css"
+  },
+  {
+    "revision": "d98351587c2fffbd7c68",
+    "url": "/static/css/1.4b44593d.chunk.css"
+  },
+  {
+    "revision": "2d4e8c2cd5965705500b93b11cfb029b",
+    "url": "/index.html"
+  }
+];
